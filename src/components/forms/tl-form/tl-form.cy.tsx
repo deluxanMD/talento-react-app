04bc3e4079b918fd8ed2a7418ef3c1543be089f9
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-01-17 22:06:56 +0530

    definitely cover

--- src/components/forms/tl-form/tl-form.cy.tsx
+++ src/components/forms/tl-form/tl-form.cy.tsx
@@ -1,11 +1,11 @@
 import { useForm } from "react-hook-form";
 import TLForm from "./tl-form.component";
 
-const Testcomponent = () => {
+const Testcomponent = ({ devTool = false }) => {
   const form = useForm();
 
   return (
-    <TLForm formMethods={form}>
+    <TLForm formMethods={form} devTool={devTool}>
       <input />
     </TLForm>
   );
@@ -15,5 +15,10 @@ describe("<TLForm />", () => {
   it("should load the form", () => {
     cy.mount(<Testcomponent />);
     cy.findByTestId("TLForm").should("exist");
+  });
+
+  it("should load dev tools", () => {
+    cy.mount(<Testcomponent devTool={true} />);
+    cy.get('button[title="Show dev panel"]').should("exist");
   });
 });
