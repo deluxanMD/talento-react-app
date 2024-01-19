4078e6bd285315eae1ad0caf494aca51ff8ee1b5
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-01-19 12:28:50 +0530

    not needed

--- src/components/forms/fields/tl-autocomplete-field/tl-autocomplete-field.hooks.ts
+++ src/components/forms/fields/tl-autocomplete-field/tl-autocomplete-field.hooks.ts
@@ -4,7 +4,7 @@ export const useOnAutocompleteChange = (field, multiple) =>
   useCallback(
     (_e, option, reason) => {
       if (multiple) {
-        field.onChange(option.map((option) => option?.id ?? option));
+        field.onChange(option.map((option) => option?.id));
       } else {
         if (reason === "clear") field.onChange("");
         else field.onChange(option.id);
