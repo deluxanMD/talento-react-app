e1878da53adc63e8392241862d34a19499516adb
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-01-19 17:58:39 +0530

    code coverage

--- src/components/forms/search-job-form/search-job-form.cy.tsx
+++ src/components/forms/search-job-form/search-job-form.cy.tsx
@@ -21,4 +21,18 @@ describe("<SearchJobForm />", () => {
       "I hope you belong to this earth ;-)"
     );
   });
+
+  it("should submit form", () => {
+    cy.mount(<SearchJobForm />);
+    cy.findByTestId("SearchJobForm.Keyword").type("Software");
+    cy.findByTestId("SearchJobForm.Country").type("Sri");
+    cy.findByTestId("SearchJobForm.Country.Listbox")
+      .findByRole("option")
+      .eq(0)
+      .click();
+    cy.findByTestId("SearchJobForm.Country")
+      .find("input")
+      .should("have.value", "Sri Lanka");
+    cy.findByTestId("TLLoadingButton").click();
+  });
 });
