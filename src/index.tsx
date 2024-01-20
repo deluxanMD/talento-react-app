d730218e6ecff687469d9508eda88615166d03ac
Deluxan Mariathasan <86180355+deluxanMD@users.noreply.github.com>
2024-01-20 21:22:21 +0530

    Mui Theme Provider (#12)
    * theme palette
    
    * theme provider component

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "store";
+import ThemeWrapper from "helpers/theme/theme.wrapper";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeWrapper>
+        <App />
+      </ThemeWrapper>
+    </Provider>
   </React.StrictMode>
 );
