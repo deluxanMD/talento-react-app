07c6748e9eebb0315fa34cd668eb2dd374fc9f32
Deluxan Mariathasan <86180355+deluxanMD@users.noreply.github.com>
2024-01-21 07:19:33 +0530

    react router dom (#16)

---
+++ src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export const HomePage = () => {
+  return (
+    <Box data-testid="HomePage.Container">
+      <Typography>Homepage</Typography>
+    </Box>
+  );
+};
