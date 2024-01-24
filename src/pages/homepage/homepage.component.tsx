6d410c81b961b62087e52a6b6f6162b59c814edb
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-01-24 21:25:12 +0530

    cypress fix

--- src/pages/homepage/homepage.component.tsx
+++ src/pages/homepage/homepage.component.tsx
@@ -1,9 +1,10 @@
+import { Box } from "@mui/material";
 import { Header } from "components/header/header.component";
 
 export const HomePage = () => {
   return (
-    <>
+    <Box data-testid="HomePage.Container">
       <Header />
-    </>
+    </Box>
   );
 };
