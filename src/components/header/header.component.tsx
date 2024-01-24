350cd5610117e366de0b5a766050324ab6daf251
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-01-24 21:04:25 +0530

    more height for small screens

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -12,7 +12,7 @@ export const Header = () => {
         p: 3,
         borderRadius: 0,
         width: "100%",
-        height: "500px",
+        height: "580px",
         backgroundImage: `url(${imageURL})`,
         backgroundPosition: "center",
         backgroundSize: "cover",
