74923b579c5678fc42335358848d2f0809948f99
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-01-25 10:47:09 +0530

    more improvements

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,26 +1,26 @@
 import { Paper, Typography } from "@mui/material";
 import { SearchJobForm } from "components/forms/search-job-form/search-job-form.component";
 import { Headline } from "components/headline/headline.component";
-import CoverImg from "../../assets/images/model-office-outfit2.png";
+import CoverImg from "../../assets/images/model-office-outfit.png";
 
 export const Header = () => {
   return (
     <Paper
       sx={{
         p: 3,
-        height: 600,
+        height: 660,
         borderRadius: 0,
         backgroundImage: `url(${CoverImg})`,
-        backgroundAttachment: "fixed",
-        backgroundSize: "1100px",
+        backgroundSize: "cover",
+        backgroundPosition: "30% 25%",
         backgroundRepeat: "no-repeat",
       }}
       data-testid="Header"
     >
       <Headline mt={4} mb={2} />
       <Typography
-        mt={10}
-        mb={10}
+        sx={{ mt: { md: 24, xs: 12 } }}
+        mb={4}
         color="GrayText"
         data-testid="Header.Description"
       >
